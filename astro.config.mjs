e4706146bdf702e7470b896dd47f3d9acc0bef2b
agusv2022 <113145066+agusv2022@users.noreply.github.com>
2026-07-14 16:33:32 +0100

    Add site URL to Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
+  site: 'https://hogargallan.com', // Cambia esto por tu dominio real
   output: 'static',
 });
