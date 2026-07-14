638c11d5cfbb302c87439eb4ea2809935a94b9e5
agusv2022 <113145066+agusv2022@users.noreply.github.com>
2026-07-14 16:43:38 +0100

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://hogargallan.com', // Cambia esto por tu dominio real
   output: 'static',
 });
